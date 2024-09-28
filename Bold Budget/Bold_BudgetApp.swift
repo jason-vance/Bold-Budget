a10b73586dc91cd8ce556cfa1e769d37efdc6a90
Jason Vance <jasonvance77@gmail.com>
2024-09-27 22:48:27 -1000

    Initial Commit

---
+++ Bold Budget/Bold_BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bold_BudgetApp.swift
+//  Bold Budget
+//
+//  Created by Jason Vance on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Bold_BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
