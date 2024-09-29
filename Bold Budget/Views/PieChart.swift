3b415dab68293fee28e1df8dd68c23b629e396ad
Jason Vance <jasonvance77@gmail.com>
2024-09-28 21:40:37 -1000

    Refactor PieChart to hopefully make the next part easier

--- Bold Budget/Views/PieChart.swift
+++ Bold Budget/Views/PieChart.swift
@@ -11,75 +11,82 @@ struct PieChart: View {
     
     private let colors: [Color] = [.red, .yellow, .blue, .purple, .orange, .green]
     
-    private struct Slice: Identifiable {
-        var id: Int { index }
-        let index: Int
+    public struct Slice {
+        let name: String
         let value: Double
-        let previousCumulativeValue: Double
     }
     
+    private struct _Slice: Identifiable {
+        var id: Int { index }
+        let index: Int
+        let name: String
+        let value: Double
+        let from: CGFloat
+        let to: CGFloat
+    }
+
     private let lineWidth: CGFloat = 16
     private var minSliceViewSize: CGFloat  { 2 * lineWidth }
     
-    @State private var pieces: [Slice] = [
-        .init(
-            index: 0,
-            value: 500,
-            previousCumulativeValue: 0
-        ),
-        .init(
-            index: 1,
-            value: 400,
-            previousCumulativeValue: 500
-        ),
-        .init(
-            index: 2,
-            value: 300,
-            previousCumulativeValue: 900
-        ),
-        .init(
-            index: 3,
-            value: 200,
-            previousCumulativeValue: 1200
-        ),
-        .init(
-            index: 4,
-            value: 10,
-            previousCumulativeValue: 1400
-        ),
-    ]
+    @State var slices: [Slice]
     
-    private var sortedSlices: [Slice] {
-        pieces.sorted { $0.index < $1.index }
+    private func makeSlices(pieSize: CGSize) -> [_Slice] {
+        let pieCircumference = min(pieSize.width, pieSize.height) * Double.pi
+        let padding = CGFloat.padding / pieCircumference
+        let lineCap = (lineWidth / 2) / pieCircumference
+        
+        var index = -1
+        var previousCumulativeValue: CGFloat = 0
+        return slices
+            .sorted { $0.value > $1.value }
+            .map { slice in
+                let from = (previousCumulativeValue / total) + lineCap + (padding / 2)
+                let to = from + (slice.value / total) - (2 * lineCap) - (padding / 2)
+                
+                previousCumulativeValue += slice.value
+                index += 1
+                
+                return .init(
+                    index: index,
+                    name: slice.name,
+                    value: slice.value,
+                    from: from,
+                    to: to
+                )
+            }
     }
     
-    private var total: Double { pieces.reduce(0, { $0 + $1.value }) }
+    private var total: Double { slices.reduce(0, { $0 + $1.value }) }
     
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                ForEach(sortedSlices) { slice in
-                    SliceView(slice, pieSize: geo.size)
+                let slices = makeSlices(pieSize: geo.size)
+                ForEach(slices) { slice in
+                    SliceView(slice)
                 }
             }
         }
         .padding(lineWidth / 2)
     }
     
-    @ViewBuilder private func SliceView(_ slice: Slice, pieSize: CGSize) -> some View {
-        let pieCircumference = min(pieSize.width, pieSize.height) * Double.pi
-        let padding = CGFloat.padding / pieCircumference
-        let lineCap = (lineWidth / 2) / pieCircumference
-        
-        let from = (slice.previousCumulativeValue / total) + lineCap + (padding / 2)
-        let to = from + (slice.value / total) - (2 * lineCap) - (padding / 2)
+    @ViewBuilder private func SliceView(_ slice: _Slice) -> some View {
         Circle()
-            .trim(from: from, to: to)
+            .trim(from: slice.from, to: slice.to)
             .rotation(.degrees(-90))
             .stroke(colors[slice.index], style: .init(lineWidth: lineWidth, lineCap: .round))
     }
 }
 
 #Preview {
-    PieChart()
+    let slices: [PieChart.Slice] = [
+        .init(name: "Groceries", value: 600),
+        .init(name: "Housing", value: 2500),
+        .init(name: "Travel", value: 1500),
+        .init(name: "Entertainment", value: 250),
+        .init(name: "Candy Bar", value: 5),
+        .init(name: "Medical", value: 125)
+    ]
+    
+    PieChart(slices: slices)
 }
